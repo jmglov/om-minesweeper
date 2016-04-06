995b30726ba3c6c97670d083396662eeeee3ebcc
James <james@magine.com>
2016-04-06 12:04:05 +0200

    Switched to using read and mutate functions

--- src/minesweeper/core.cljs
+++ src/minesweeper/core.cljs
@@ -3,20 +3,46 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
+;;; --- Configuration ---
+
 (enable-console-print!)
 
-(def mine-percentage 0)
+(def mine-percentage 0.10)
 
-(def minefield-width 2)
-(def minefield-height 2)
+(def minefield-width 8)
+(def minefield-height 8)
 
-(defn mine? []
+;;; --- State ---
+
+(defn generate-mine? []
   (< (rand) mine-percentage))
 
 (defn generate-minefield []
- (vec (for [_ (range minefield-height)]
+  (vec (for [_ (range minefield-height)]
         (vec (for [_ (range minefield-width)]
-               {:mine? (mine?) :flipped? false})))))
+               {:mine? (generate-mine?) :flipped? false})))))
+
+(def app-state
+  (atom {:minefield (generate-minefield)}))
+
+(defn read [{:keys [state] :as env} key params]
+  (let [st @state]
+    (if-let [[_ value] (find st key)]
+      {:value value}
+      {:value :not-found})))
+
+(defn mutate [{:keys [state] :as env} key {:keys [x y] :as params}]
+  (if (= `flip key)
+    {:value {:keys [:minefield]}
+     :action #(swap! state update-in [:minefield x y :flipped?] (constantly true))}
+    {:value :not-found}))
+
+(def reconciler
+  (om/reconciler
+   {:state app-state
+    :parser (om/parser {:read read :mutate mutate})}))
+
+;;; --- Helpers ---
 
 (defn neighbour-coords [minefield [x y]]
   (for [cand-x [(dec x) x (inc x)]
@@ -32,57 +58,58 @@
        (filter #(:mine? (get-in minefield %)))
        count))
 
-(def app-state
-  (atom (generate-minefield)))
-
 (defn lost? []
   (some #(and (:flipped? %) (:mine? %)) (flatten @app-state)))
 
 (defn won? []
   (every? #(or (:flipped? %) (:mine? %)) (flatten @app-state)))
 
-(defn flip-cell! [minefield [x y]]
-  (swap! app-state update-in [x y :flipped?] (constantly true))
-  (when (= (count-adjacent-mines minefield [x y]) 0)
-    (doseq [neighbour-coord (neighbour-coords minefield [x y])]
-      (when (not= :flipped? (get-in @app-state [x y]))
-        (flip-cell! @app-state neighbour-coord)))))
+(defn flip-all-zero-mine-neighbours [this minefield coord]
+  (when (= (count-adjacent-mines minefield coord) 0)
+    (doseq [neighbour-coord (neighbour-coords minefield coord)]
+      (when (not= :flipped? (get-in @app-state neighbour-coord))
+        (let [[x y] neighbour-coord]
+          (om/transact! this `[(flip {:x ~x, :y ~y})]))))))
 
 (defn cell-text [minefield coord]
   (let [cell (get-in minefield coord)]
+   (println cell)
    (cond
     (not (:flipped? cell)) "_"
     (not (:mine? cell)) (count-adjacent-mines minefield coord)
     (:mine? cell) "☠")))
 
-(defn make-button [minefield cell x y]
+(defn make-button [this minefield cell x y]
   (dom/button
    #js {:onClick
         (fn [e]
-          (flip-cell! minefield [x y])
-          (if (lost?) (js/alert "You lost!")
-            (when (won?) (js/alert "You won!"))))}
+          (om/transact! this `[(flip {:x ~x, :y ~y})])
+          (flip-all-zero-mine-neighbours this minefield [x y]))}
    (cell-text minefield [x y])))
 
-(defn make-cell [minefield x y]
+(defn make-cell [this minefield x y]
   (let [cell (-> minefield (nth x) (nth y))]
-    (make-button minefield cell x y)))
+    (make-button this minefield cell x y)))
 
-(defn make-row [minefield x]
+(defn make-row [this minefield x]
   (dom/div nil
     (->> (range (count (nth minefield x)))
-         (map #(make-cell minefield x %)))))
+         (map #(make-cell this minefield x %)))))
+
+;;; --- React component ---
 
 (defui Minefield
+  static om/IQuery
+  (query [this]
+    [:minefield])
   Object
   (render [this]
-    (let [minefield (om/props this)]
+    (let [{:keys [minefield]} (om/props this)]
       (dom/div nil
         (->> (range (count minefield))
-             (map #(make-row minefield %)))))))
+             (map #(make-row this minefield %)))))))
 
-(def reconciler
-  (om/reconciler {:state app-state}))
+;;; --- App ---
 
 (om/add-root! reconciler
   Minefield (gdom/getElement "app"))
